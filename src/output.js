880ab57d5bdf429c5aaa887883c050970a19c617
nadeem-mmd <nadim414@gmail.com>
2020-08-13 20:11:26 +0530

    adding test files

---
+++ src/output.js
@@ -0,0 +1,58 @@
+const output = {
+    "payload": [{
+
+        "headerName": "Big & Tall",
+        "sizes": [{
+            "value": "L, Tall",
+            "selected": true
+        }, {
+            "value": "XL, Big"
+        }, {
+            "value": "XL, Tall",
+            "selected": true
+        }, {
+            "value": "2XL, Big"
+        }]
+    }, {
+        "headerName": "Women's Regular",
+        "sizes": [{
+            "value": "1, S",
+            "selected": true
+        }, {
+            "value": "2, S",
+            "selected": true
+        }, {
+            "value": "3, S"
+        }, {
+            "value": "4, S"
+        }]
+    }, {
+        "headerName": "Kid's Regular",
+        "sizes": [{
+            "value": "S",
+            "selected": true
+        }, {
+            "value": "M",
+            "selected": true
+        }, {
+            "value": "XS"
+        }, {
+            "value": "XXS"
+        }]
+    }, {
+        "headerName": "Kid's Regular",
+        "sizes": [{
+            "value": "S",
+            "selected": true
+        }, {
+            "value": "M",
+            "selected": true
+        }, {
+            "value": "XS"
+        }, {
+            "value": "XXS"
+        }]
+    }]
+}
+
+export default output;
